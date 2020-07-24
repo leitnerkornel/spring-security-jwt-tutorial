8da0e1db2ca7ec02f2d9dcac51150996c63f3e9e
Kornél <leitner.kornel@gmail.com>
2020-07-24 16:05:19 +0200

    New route ("/me") allowed for authenticated users only

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -7,9 +7,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public SecurityConfig(JwtTokenServices jwtTokenServices) {
+        this.jwtTokenServices = jwtTokenServices;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,7 +29,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth/signin").permitAll()
                 .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated()
                 .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN")
-                .anyRequest().denyAll();
+                .antMatchers(HttpMethod.GET, "/me").authenticated()
+                .anyRequest().denyAll()
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenServices),
+                        UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
